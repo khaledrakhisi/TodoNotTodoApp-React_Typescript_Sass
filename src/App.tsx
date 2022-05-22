49b817ae4f5dda9b0188a80fad19c63b7fbbd261
khaledr <info@khaledr.ir>
2022-05-22 20:36:07 +0430

    refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 
-import Header from "./components/Header";
+import { Header } from "./components/Header";
 import { MainPage } from "./pages/MainPage";
 
 import "../src/scss/App.scss";
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import { SearchBox } from "./SearchBox";
 
 import classes from "./Header.module.scss";
 
-const Header = () => {
+export const Header = () => {
   return (
     <header className={classes.mainHeader}>
       <h2 className="title"> Todo and not Todo </h2>
@@ -10,5 +10,3 @@ const Header = () => {
     </header>
   );
 };
-
-export default Header;
